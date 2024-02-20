7287b46948b83116a2c6deffd79d793a2518a4e0
AnthonyB0511 <137073922+AnthonyB0511@users.noreply.github.com>
2024-02-20 18:57:32 +0100

    :sparkles: :feat ajout de la modal

--- components/PersonBlock.js
+++ components/PersonBlock.js
@@ -1,28 +1,34 @@
-import React, { useState, useRef } from 'react';
-import { View, Text, TouchableOpacity, TextInput } from 'react-native';
+import { useState, useRef, useEffect } from 'react';
+import { View, Text, TouchableOpacity, TextInput, Modal, Alert } from 'react-native';
+import CustomButtonValidation from './CustomButtonValidation';
 
 const PersonBlock = ({ name, color }) => {
     const [score, setScore] = useState(0);
     const [inputValue, setInputValue] = useState('');
     const [showInput, setShowInput] = useState(false);
     const [operationValue, setOperationValue] = useState(0);
-
+    const [modalVisible, setModalVisible] = useState(false);
+    const [modalInputValue, setModalInputValue] = useState('');
     const inputRef = useRef(null);
+
+    const handleShortPress = (value) => {
+        setScore(score + value);
+    };
 
     const handleLongPress = (value) => {
         setOperationValue(value);
-        setInputValue('');
-        setShowInput(true);
-        inputRef.current.focus(); // Focusing the input when long pressed
+        setModalVisible(true);
     };
-
-    const handleInputConfirm = () => {
-        const newValue = parseInt(inputValue);
-        if (!isNaN(newValue)) {
-            setScore(score + (operationValue * newValue));
+    useEffect(() => {
+        if (!modalVisible) {
+            setModalInputValue('');
         }
-        setInputValue('');
-        setShowInput(false);
+    }, [modalVisible]);
+
+
+    const handleModalButtonPress = (value) => {
+        setScore(score + value);
+        setModalVisible(false);
     };
 
     return (
@@ -30,31 +36,52 @@ const PersonBlock = ({ name, color }) => {
             <Text style={{ color: '#242F40', fontWeight: 900, marginRight: 15, fontSize: 24 }}>{name}</Text>
             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                 <TouchableOpacity
-                    onPress={() => setScore(score - 1)}
+                    onPress={() => handleShortPress(-1)}
                     onLongPress={() => handleLongPress(-1)}
                     style={{ backgroundColor: color, padding: 10, borderRadius: 5, marginRight: 10, width: 40 }}>
                     <Text style={{ color: '#242F40', textAlign: 'center', fontWeight: 900, fontSize: 24 }}>-</Text>
                 </TouchableOpacity>
-                {showInput ? (
-                    <TextInput
-                        ref={inputRef}
-                        value={inputValue}
-                        onChangeText={setInputValue}
-                        onBlur={handleInputConfirm}
-                        onSubmitEditing={handleInputConfirm}
-                        keyboardType="numeric"
-                        style={{ borderWidth: 1, borderColor: '#242F40', borderRadius: 5, paddingHorizontal: 5, width: 50, textAlign: 'center' }}
-                    />
-                ) : (
-                    <Text style={{ color: '#242F40', fontWeight: 900, width: 50, textAlign: 'center', fontSize: 22 }}>{score}</Text>
-                )}
+                <Text style={{ color: '#242F40', fontWeight: 900, width: 50, textAlign: 'center', fontSize: 22 }}>{score}</Text>
                 <TouchableOpacity
-                    onPress={() => setScore(score + 1)}
+                    onPress={() => handleShortPress(1)}
                     onLongPress={() => handleLongPress(1)}
                     style={{ backgroundColor: color, padding: 10, borderRadius: 5, marginLeft: 10, width: 40 }}>
                     <Text style={{ color: '#242F40', textAlign: 'center', fontWeight: 900, fontSize: 24 }}>+</Text>
                 </TouchableOpacity>
             </View>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => setModalVisible(false)}
+            >
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#242F40' }}>
+                    <View style={{ backgroundColor: '#242F40', padding: 20, borderRadius: 10, width: 300, justifyContent: 'center', borderColor: '#FBAC23', border: 1 }}>
+                        <CustomButtonValidation title={`${operationValue * 5}`} onPress={() => handleModalButtonPress(operationValue * 5)} />
+                        <CustomButtonValidation title={`${operationValue * 10}`} onPress={() => handleModalButtonPress(operationValue * 10)} />
+                        <CustomButtonValidation title={`${operationValue * 15}`} onPress={() => handleModalButtonPress(operationValue * 15)} />
+                        <CustomButtonValidation title={`${operationValue * 20}`} onPress={() => handleModalButtonPress(operationValue * 20)} />
+                        <View style={{ flexDirection: 'row', justifyContent: 'center', marginBottom: 10 }}>
+                            <TextInput
+                                value={modalInputValue}
+                                onChangeText={setModalInputValue}
+                                keyboardType="numeric"
+                                placeholder="Autre valeur"
+                                placeholderTextColor='#FBAC23'
+                                style={{ borderWidth: 1, color: '#FBAC23', borderColor: '#FBAC23', borderRadius: 5, paddingHorizontal: 5, marginTop: 10, width: 240, textAlign: 'center', fontSize: 20 }}
+                            />
+                        </View>
+
+                        <CustomButtonValidation title="Confirmer" onPress={() => {
+                            if (modalInputValue === '') {
+                                Alert.alert('Erreur', 'Veuillez saisir une valeur.');
+                            } else {
+                                handleModalButtonPress(operationValue * parseInt(modalInputValue));
+                            }
+                        }} />
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
 };
